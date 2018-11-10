9efae32d45a3ab1bcd51a074d773e60380f116c9
jonathannah <44736673+jonathannah@users.noreply.github.com>
2018-11-09 23:14:33 -0800

    New class to read user from table

---
+++ lib/User.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dronca
+ * Date: 11/9/18
+ * Time: 10:46 PM
+ */
+
+include_once "DBHelper.php";
+
+class User
+{
+    public $fname;
+    public $lname;
+    public $email;
+    public $password;
+    public $group;
+
+    public static function fromQuery($uid)
+    {
+        $dbh = new DBHelper();
+        $query = "SELECT lastName, firstName, emailAddress, password, groupID FROM  `User` WHERE 'emailAddress' = $uid";
+        $result = $dbh->query($query);
+
+        $row = mysqli_fetch_array($result);
+        return self::fromRow($row);
+        $dbh->close();
+    }
+
+    public static function fromRow($row)
+    {
+        $user = new User();
+
+        $user->fname = $row["firstName"];
+        $user->lname = $row["lastName"];
+        $user->email = $row["emailAddress"];
+        $user->password = $row["password"];
+        $user->group = $row["groupID"];
+
+        return $user;
+    }
+
+
+}
